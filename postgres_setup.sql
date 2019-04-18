e1beb71ca97ffd10beda2593aa55899238c91fce
Michael Goff <msgoff@users.noreply.github.com>
2019-04-18 22:47:37 +0000

    db setup

---
+++ postgres_setup.sql
@@ -0,0 +1,5 @@
+create user selective_shop with password 'selective_shop';  
+create database selective_shop;  
+create database selective_shop_test;  
+alter database selective_shop owner to selective_shop;  
+alter database selective_shop_test owner to selective_shop;  
