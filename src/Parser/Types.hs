e094a3a01c44bf7a7966cba15639934118fe8d5a
Dmitri Schuiski <errfrom@yandex.ru>
2019-04-18 21:44:08 +0300

    ResourceClass Definition.

--- src/Parser/Types.hs
+++ src/Parser/Types.hs
@@ -1,1 +1,23 @@
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TypeFamilies           #-}
+
 module Parser.Types where
+
+data User -- TODO: UserM monad
+
+-- Each source produces its own elements that could be restricted by particular parameters.
+-- Source specifies where the elements are.
+-- Parameters specify which elements to extract.
+class ResourceClass source element | source -> element where
+  data ResourceParameters element :: *
+  requestElements :: (Monad m) => source -> ResourceParameters element -> m [element]
+
+-- TODO: resource combinations
+-- 2 resources with the same resourceSource could be combined
+-- that combination will force a combination of their correspondent parameters
+
+data Monitor source = Monitor
+  { monitorUser :: User
+  , monitorTarget :: source
+  , monitorInterval :: Int }
