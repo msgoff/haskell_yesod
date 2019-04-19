3a9fae3d1c54ce4a52526abd3214dc4d6c071fa2
Michael Goff <msgoff@users.noreply.github.com>
2019-04-18 21:03:48 -0400

    Update debian_9.sh

--- debian_9.sh
+++ debian_9.sh
@@ -1,6 +1,8 @@
-http://postgrest.org/en/v5.2/install.html#build-from-source 
-git clone https://github.com/PostgREST/postgrest.git
-cd postgrest
 
-# adjust local-bin-path to taste
-stack build --install-ghc --copy-bins --local-bin-path /usr/local/bin
+#build-from-source 
+#http://postgrest.org/en/v5.2/install.html 
+git clone https://github.com/PostgREST/postgrest.git 
+cd postgrest 
+
+# adjust local-bin-path to taste 
+stack build --install-ghc --copy-bins --local-bin-path /usr/local/bin 
