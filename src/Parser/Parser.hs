4738cb0e9fc9b744038f397db49ccec3a0461658
Dmitri Schuiski <errfrom@yandex.ru>
2019-05-02 18:13:52 +0300

    Parser Core adapted to Item model.

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-orphans        #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE FlexibleInstances    #-}
@@ -5,22 +8,19 @@
 
 module Parser.Parser where
 
-import           Control.Monad                      (forever)
 import           Control.Monad.IO.Class             (liftIO)
 import           Control.Monad.Trans.Reader         (ReaderT)
 import           Control.Exception                  (catch)
-import           Control.Concurrent                 (threadDelay)
-import           Data.IORef                         (newIORef, readIORef, writeIORef)
-import qualified Data.Text                  as T    (unpack, pack, splitOn)
+import qualified Data.Text                  as T    (pack)
 import           Data.Text                          (Text)
 import qualified Data.ByteString.Lazy.Char8 as BSL  (unpack)
-import qualified Data.Set                   as S
-import           Data.Set                           (Set)
+import qualified Data.Maybe                 as M    (isJust)
+import qualified Data.Char                  as C    (toUpper)
 import qualified Data.Time.Clock.POSIX      as Time (posixSecondsToUTCTime)
 
 import           Control.Lens
 import           Data.Aeson
-import           Data.Aeson.Types                   (Value(..), parseMaybe)
+import           Data.Aeson.Types                   (Value(..), Parser, parseMaybe)
 import qualified Network.Wreq               as R
 import           Network.Wreq                       (Response, JSONError(..))
 
@@ -29,67 +29,71 @@ import qualified Database.Persist.Class     as P    (insertUnique)
 import qualified Database.Persist.Sql       as P    (runSqlPool)
 import           Database.Persist.Sql               (SqlBackend)
 
-import           Model                      hiding  (JobId)
+import           Model
 import           Parser.Types
 
 default (Text)
 
-type JobURL = String
-type JobId  = Text
+type URL = String
+type MaxItemId  = Int
 
-monitorJobs :: Interval -> Pool SqlBackend -> IO ()
-monitorJobs interval connPool =
-  let baseUrl        = "https://hacker-news.firebaseio.com/v0/jobstories.json"
-  in do
-    storedIdsIORef <- newIORef S.empty
-    forever $ do
-      storedIds <- readIORef storedIdsIORef
-      jobIds <- receiveJobIds baseUrl
-      let newJobIds = S.difference (S.fromList jobIds) storedIds
-      receivedJobIds <- flip P.runSqlPool connPool $ worker (S.toList newJobIds)
-      writeIORef storedIdsIORef (S.union storedIds receivedJobIds)
-      threadDelay (interval * 1000000)
-  where worker :: [JobId] -> ReaderT SqlBackend IO (Set JobId)
-        worker [] = return S.empty
-        worker (jobId:xs) =
-          let url = "https://hacker-news.firebaseio.com/v0/item/" ++ (T.unpack jobId) ++ ".json"
-          in do job <- liftIO $ parseJob url
-                case job of
-                  Nothing -> worker xs
-                  Just job' -> do
-                    P.insertUnique job'
-                    rest <- worker xs
-                    return $ S.insert jobId rest
+-- Asks for the current largest item id and
+-- makes a particular number of steps backward
+-- from that point.
+discoverItems :: Int -> Pool SqlBackend -> IO ()
+discoverItems itemsAmount connPool = do
+  maxitemId <- getCurrentMaxitem
+  flip P.runSqlPool connPool $ worker itemsAmount maxitemId
+  where worker :: Int -> MaxItemId -> ReaderT SqlBackend IO ()
+        worker 0 _ = return ()
+        worker counter itemId =
+          let url = "https://hacker-news.firebaseio.com/v0/item/" ++ show itemId ++ ".json"
+          in do item <- liftIO $ parseItem url
+                let go = worker (pred counter) (pred itemId)
+                case item of
+                  Nothing -> go -- TODO: check if in the database + additional param
+                  Just item' -> P.insertUnique item' >> go -- TODO: one-to-many kids
 
-receiveJobIds :: String -> IO [JobId]
-receiveJobIds url = do
-  resp <- R.get url
-  let respBody = BSL.unpack (resp ^. R.responseBody)
-  let jobIds   = T.splitOn "," (T.pack . tail . init $ respBody)
-  -- FIXME: That is not a safe way to decode a json.
-  --        JSON must be parsed to a list of strings with Aeson.
-  return jobIds
+getCurrentMaxitem :: IO MaxItemId
+getCurrentMaxitem =
+  let url = "https://hacker-news.firebaseio.com/v0/maxitem.json"
+  in do resp <- R.get url
+        let maxitemId = read . BSL.unpack $ resp ^. R.responseBody
+        return maxitemId
 
-instance FromJSON (JobURL -> Job) where
+
+instance FromJSON (URL -> Item) where
   parseJSON (Object obj) = do
-    jobUsername <- obj .: "by"
-    jobScore <- obj .: "score"
-    jobCreated <- toUTC <$> obj .: "time"
-    jobTitle <- obj .: "title"
-    jobDescription <- obj .: "text"
-    return $ \jobUrl' -> let jobUrl = T.pack jobUrl' in Job{..}
-    where toUTC = Time.posixSecondsToUTCTime
+    itemApiId <- obj .: "id"
+    itemDeleted <- obj `getBool` "deleted"
+    itemTypeText <- obj .: "type"
+    let itemItemType = read $ (C.toUpper . head $ itemTypeText) : tail itemTypeText :: ItemType
+    itemUsername <- obj .: "by"
+    itemCreated <- toUTC <$> obj .: "time"
+    itemText <- obj .:? "text"
+    itemDead <- obj `getBool` "dead"
+    itemParent <- obj .:? "parent"
+    itemPool <- obj .:? "pool"
+    itemScore <- obj .: "score"
+    itemTitle <- obj .:? "title"
+    return $ \strItemUrl -> let itemUrl = T.pack strItemUrl in Item{..}
+
+    where getBool obj key = do
+            v <- obj .:? key :: Parser (Maybe Text)
+            return $ M.isJust v
+          toUTC = Time.posixSecondsToUTCTime
   parseJSON _ = error "Not an object."
 
-parseJob :: JobURL -> IO (Maybe Job)
-parseJob url = do
+
+parseItem :: URL -> IO (Maybe Item)
+parseItem url = do
   mResp <- flip catch (\(JSONError _) -> return Nothing) $
              Just <$> (R.asJSON =<< R.get url :: IO (Response Object))
   return $
     case mResp of
       Nothing -> Nothing
       Just resp -> let respBody = resp ^. R.responseBody
-                       toJob    = parseMaybe parseJSON (Object respBody)
-                   in case toJob of
+                       toItem   = parseMaybe parseJSON (Object respBody)
+                   in case toItem of
                         Nothing -> Nothing
                         Just f -> Just (f url)
